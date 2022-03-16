f3cfd7c2dc855bfb7ff25f0aa92d440738ef500a
Javi Santana <qualopec@gmail.com>
2022-03-16 20:46:01 +0100

    added tests

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 
 import unittest
 import requests
+import os
 
 class testTest(unittest.TestCase):
 
     def test_call(self):
         token = os.getenv('TB_TOKEN')
-        r = requests.get('https://api.tinybird.co/v0/pipes/test.json?token={token}')
+        r = requests.get(f'https://api.tinybird.co/v0/pipes/test.json?token={token}')
         self.assertEqual(r.status_code, 200)
                 
